024706d1720c03321684191d4ff02be99a4a55a3
Jiahua Wang <jiahuawang7@gmail.com>
2020-10-22 21:06:56 -0700

    use new resume format

---
+++ _next/static/MMMMMMMMMMMMMMMMMMMMM/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002F29107295.4e5e9f49293217e29f19.js","static\u002Fchunks\u002Fpages\u002Findex-b37fc8e583dd79164a0e.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-5efcade778114a255845.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
